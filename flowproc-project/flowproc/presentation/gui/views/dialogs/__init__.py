0c574885ca2c31690ce7898eeb5a880ad8860c23
Lime-3 <trove.files_9d@icloud.com>
2025-08-13 18:05:48 -0400

    gui: refactor VisualizationDialog into modular components; add VisualizationOptions and visualization_filters; remove legacy VisualizationDisplayDialog; update event handler and exports; DRY population extraction

--- flowproc-project/flowproc/presentation/gui/views/dialogs/__init__.py
+++ flowproc-project/flowproc/presentation/gui/views/dialogs/__init__.py
@@ -5,7 +5,8 @@ Dialog components for the GUI.
 from .group_labels_dialog import GroupLabelsDialog
 from .manual_groups_dialog import ManualGroupsDialog
 from .preview_dialog import PreviewDialog
-from .visualization_dialog import VisualizationDialog, VisualizationOptions
+from .visualization_dialog import VisualizationDialog
+from .visualization_options import VisualizationOptions
 
 __all__ = [
     'GroupLabelsDialog',
